13f8eca59c1b5ca0d338d24d8be9c274cddacce5
SAYALI JADHAV <jadhavsayalip2316@gmail.com>
2023-06-18 19:44:07 +0530

    2nd commete

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ const resume_btn2 = document.querySelector("#resume-button-2");
 resume_btn1.addEventListener("click", (e) => {
   e.preventDefault();
   window.open(
-    "https://drive.google.com/file/d/1yz24CNYgOW3KnvhAzCNm2i4d7mkrf9Du/view?usp=drive_link"
+    "https://drive.google.com/file/d/1o5NmeaszbWUO70uMHt1EP3sQuF0iaTuQ/view?usp=sharing"
   );
   window.location.href =
-    "https://drive.google.com/uc?export=download&id=1yz24CNYgOW3KnvhAzCNm2i4d7mkrf9Du";
+    "https://drive.google.com/uc?export=download&id=1o5NmeaszbWUO70uMHt1EP3sQuF0iaTuQ";
 });
 
 resume_btn2.addEventListener("click", (e) => {
   e.preventDefault();
   window.open(
-    "https://drive.google.com/file/d/1yz24CNYgOW3KnvhAzCNm2i4d7mkrf9Du/view?usp=drive_link"
+    "https://drive.google.com/file/d/1o5NmeaszbWUO70uMHt1EP3sQuF0iaTuQ/view?usp=sharing"
   );
   window.location.href =
-    "https://drive.google.com/uc?export=download&id=1yz24CNYgOW3KnvhAzCNm2i4d7mkrf9Du";
+    "https://drive.google.com/uc?export=download&id=1o5NmeaszbWUO70uMHt1EP3sQuF0iaTuQ";
 });
